eb5de91c80e9534e4e13366f238cd7ce4d5d6ed1
marcopisani002 <pisanimarco34@gmail.com>
2023-02-02 17:56:21 +0100

    fix bugs on edit page

--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -14,20 +14,20 @@
 </div>
 @endif
 
-<form action="{{ route('movies.store') }}" method="POST">
+<form action="{{ route('movies.store') }}" method="POST" class="text-white px-3">
     @csrf
 
-    <label class="form-label">Movie title:</label>
+    <label class="form-label">Titolo:</label>
     <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('title') }}">
 
-    <label class="form-label">Description:</label>
+    <label class="form-label">Descrzione:</label>
     <textarea name="description" cols="30" rows="5" class="form-control @error('description') is-invalid @enderror">{{ old('descrizione') }}</textarea>
 
-    <label class="form-label">year of release:</label>
+    <label class="form-label">Anno d'uscita:</label>
     <input type="text" name="anno_uscita" class="form-control @error('anno_uscita') is-invalid @enderror" value="{{ old('anno_uscita') }}">
 
-    <label class="form-label">genre:</label>
-    <input type="text" name="genre" class="form-control @error('genre') is-invalid @enderror" value="{{ old('genre') }}">
+    <label class="form-label">Genere:</label>
+    <input type="text" name="genere" class="form-control @error('genere') is-invalid @enderror" value="{{ old('genere') }}">
 
     <div class="form-check form-switch">
         <input class="form-check-input" type="checkbox" role="switch" id="switch_public" name="disponibile" {{ old('disponibile', $movie->disponibile) ? 'checked' : '' }}>
@@ -36,7 +36,7 @@
 
     <button type="submit" class="btn btn-success my-3">Create</button>
 
-    <a href="{{ route('movies.index') }}" class="mx-3">Return to movies list</a>
+    <a href="{{ route('movies.index') }}" class="mx-3 btn btn-danger">Return to movies list</a>
 
 </form>
 @endsection
