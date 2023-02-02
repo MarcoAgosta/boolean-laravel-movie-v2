1ec1303f6a3bedc8a50d94a3685a14ca4fbebc39
marcopisani002 <pisanimarco34@gmail.com>
2023-02-02 17:09:50 +0100

    add web routes

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified'])
+    ->prefix("admin") // porzione di uri che verrà inserita prima di ogni rotta
+    ->name("admin.") // porzione di testo inserita prima del name di ogni rotta
+    ->group(function () {
+        Route::get('/', [ProjectController::class, "home"])->name('dashboard');
+        Route::get('/users', [ProjectController::class, "home"])->name('users');
+        Route::get('/pippo', [ProjectController::class, "home"])->name('pippo');
+
+        Route::resource("posts", PostController::class);
+    });
+require __DIR__ . '/auth.php';
