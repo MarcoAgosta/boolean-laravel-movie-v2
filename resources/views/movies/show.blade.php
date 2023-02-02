d82b41639bda5d4ec7293f346fbf2c93608f3c86
FilippoBid <FilippoPerrone1997@gmail.com>
2023-02-02 17:34:06 +0100

    modifica file show

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-   <h1 class="my-3">Progetto id #{{ $project->id }}</h1>
+   <h1 class="my-3">Movie id #{{ $movie->id }}</h1>
 
    <div class="row">
       <div class="col">
-         <img src="{{ $project->cover_img }}" alt="..." class="img-fluid">
+         <img src="{{ $movie->cover_img }}" alt="..." class="img-fluid">
       </div>
       <div class="col-8">
-         <h4>Name: </h4>
-         <p>{{ $project->name }}</p>
+         <h4>Titolo: </h4>
+         <p>{{ $movie->title }}</p>
 
-         <h4>Description: </h4>
-         <p>{{ $project->description }}</p>
+         <h4>Descrizione: </h4>
+         <p>{{ $movie->descrizione }}</p>
 
-         <h6 class="text-center">
-            <a href="{{ $project->github_link }}">Go check it on github!</a>
-         </h6>
+         <h4>Anno di uscita:{{ $movie->anno_uscita }}</h4>
+
+         <h4>Genere:{{ $movie->genere }}</h4>
+         
       </div>
    </div>
-   <a href="{{ route('admin.projects.index') }}" class="mx-3">Return to projects list</a>
-   <a href="{{ route('admin.projects.edit', $project->id) }}" class="mx-3">Edit this project</a>
+   <a href="{{ route('movies.index') }}" class="mx-3">lista dei film</a>
+   <a href="{{ route('movies.edit', $project->id) }}" class="mx-3">cambia il film</a>
 @endsection
